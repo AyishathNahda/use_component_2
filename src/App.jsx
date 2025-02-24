43e1fa76a54d3a49114f21347a6a99f24c32070e
kalvium <kalvium@kalvium.community>
2025-02-24 12:39:16 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import PostCard from './components/postcard'
 import './App.css'
 
 function App() {
@@ -30,10 +31,18 @@ function App() {
   ];
 
   return (
-    <>
+    <div className="app">
+      <h1>Social Media Feed</h1>
+      <div className="post-list">
+        {initialPosts.map((post)=>(
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+
+      </div>
       
-    </>
-  )
-}
+    
+  );
+};
 
 export default App
